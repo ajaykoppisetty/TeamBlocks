06f36e68a7f234dd440a588446384a503b590595
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-05-13 20:27:48 +0200

    Simple login

---
+++ app/src/main/java/org/faudroids/doublestacks/ui/MainActivity.java
@@ -0,0 +1,125 @@
+package org.faudroids.doublestacks.ui;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
+import org.faudroids.doublestacks.R;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+import timber.log.Timber;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends RoboActivity implements
+		GoogleApiClient.ConnectionCallbacks,
+		GoogleApiClient.OnConnectionFailedListener {
+
+	private static final int REQUEST_LOGIN = 42;
+
+	@InjectView(R.id.login_button) SignInButton loginButton;
+
+	private boolean resolvingConnectionFailure = false;
+	private boolean autoStartLoginFlow = true;
+	private boolean loginClicked = false;
+
+
+	private GoogleApiClient googleApiClient;
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		googleApiClient = new GoogleApiClient.Builder(this)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.addApi(Games.API)
+				.addScope(Games.SCOPE_GAMES)
+				.build();
+
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				login();
+			}
+		});
+	}
+
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		googleApiClient.connect();
+	}
+
+
+	@Override
+	public void onStop() {
+		googleApiClient.disconnect();
+		super.onStop();
+	}
+
+
+	@Override
+	public void onConnected(Bundle bundle) {
+		// TODO player is signed in, proceed?
+		Timber.d("Google login successful");
+	}
+
+
+	@Override
+	public void onConnectionSuspended(int i) {
+		// nothing to do for now
+	}
+
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		Timber.d("onConnectionFailed (" + connectionResult.getErrorCode() + ")");
+
+		// already resolving?
+		if (resolvingConnectionFailure) return;
+
+		// check for if login should be handled
+		if (loginClicked || autoStartLoginFlow) {
+			autoStartLoginFlow = false;
+			loginClicked = false;
+			resolvingConnectionFailure = true;
+
+			// Attempt to resolve the connection failure using BaseGameUtils.
+			if (!BaseGameUtils.resolveConnectionFailure(
+					this,
+					googleApiClient,
+					connectionResult,
+					REQUEST_LOGIN,
+					getString(R.string.error_login_other))) {
+
+				resolvingConnectionFailure = false;
+			}
+		}
+
+		// Put code here to display the sign-in button
+	}
+
+
+	private void login() {
+		loginClicked = true;
+		googleApiClient.connect();
+	}
+
+
+	// TODO use somewhere
+	private void logout() {
+		loginClicked = false;
+		Games.signOut(googleApiClient);
+	}
+
+}
---
+++ baseGameUtils/README.md
@@ -0,0 +1,1 @@
+This library has been copied from https://github.com/playgameservices/android-basic-samples
